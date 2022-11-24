00070b1a47c354eb426d28185b7e9cf2e54f6b58
RaduStefan30 <radustefan3010@gmail.com>
2022-11-24 21:35:05 +0200

    create fetch requests

---
+++ src/service.ts
@@ -0,0 +1,23 @@
+export const fetchAllCurencies = async () => {
+  const response = await fetch(
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+  );
+  const data = await response.json();
+  return data;
+};
+
+export const fetchCurrencyRates = async (currency: string) => {
+  const response = await fetch(
+    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+  );
+  const data = await response.json();
+  return data;
+};
+
+export const fetchCurrencyByDate = async (currency: string, date: string) => {
+  const response = await fetch(
+    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/${date}/currencies/${currency}.json`
+  );
+  const data = await response.json();
+  return data;
+};
